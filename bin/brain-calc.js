85cbfb4c84c45594c4278de4cfe212cfc658ba8b
RDV2210 <altbio22@yandex.ru>
2023-01-12 14:42:26 +0300

    brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainCalc from '../src/brain-calc.js';
+
+brainCalc();
---
+++ src/brain-calc.js
@@ -0,0 +1,38 @@
+import rendomNamber from './helper.js';   
+import userHello from './index.js'; 
+
+const rulesGame = 'What is the result of the expression?'; // вопрос игры 
+
+const arr = ['+', '-', '*']; // массив из операторов
+
+const randomElement = arr[Math.floor(Math.random() * arr.length)]; // полч рендомный элемент 
+const firstNamber = rendomNamber(0, 50); // полчаем рендомное первое число
+const secondNamber = rendomNamber(0, 50); // получаем рендомное второе число
+
+const calculate = (operator, firstNamber, secondNamber) => { // вугкция сложения первого ив торго числа
+  switch (operator) {
+    case '+': 
+      return firstNamber + secondNamber;
+    case '-':
+      return firstNamber - secondNamber;
+    case '*':
+      return firstNamber * secondNamber;
+    default:
+      return null;
+  }
+};
+
+const gameRendom = () => {
+  const randomElement = arr[Math.floor(Math.random() * arr.length)]; // полч рендомный элемент 
+  const firstNamber = rendomNamber(0, 50); // полчаем рендомное первое число
+  const secondNamber = rendomNamber(0, 50); // получаем рендомное второе число
+
+  return [`${firstNamber}${randomElement}${secondNamber}`, 
+  String(calculate(operator, firstNamber, secondNamber))]
+};
+
+function brainCalc() {
+  userHello(rulesGame, gameRendom);
+};
+
+export default brainCalc;
