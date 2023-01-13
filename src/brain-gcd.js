e9cd8ff92f5e1ebd43ba77405ce91afa54808341
RDV2210 <altbio22@yandex.ru>
2023-01-13 13:14:49 +0300

    brain-even finised

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -3,3 +3,13 @@ import userHello from './index.js';
 
 const rulesGame = 'Find the greatest common divisor of given numbers.'; // правило игры 
 
+const firstNamber = rendomNamber(0, 50);  // получ первое число
+const secondNamber = rendomNamber(0, 50); // получ второе число
+
+
+function NOD (firstNamber, secondNamber) { // функция считает наибольший делитель 
+	if (secondNamber > firstNamber) return NOD(secondNamber, firstNamber);
+	if (!secondNamber) return firstNamber;
+	return NOD(secondNamber, firstNamber % secondNamber);
+};
+
