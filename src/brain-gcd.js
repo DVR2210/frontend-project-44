85f2dda122659c162af355cfacb3c5f6d94cf645
RDV2210 <altbio22@yandex.ru>
2023-01-14 09:52:14 +0300

    dey com

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -3,13 +3,26 @@ import userHello from './index.js';
 
 const rulesGame = 'Find the greatest common divisor of given numbers.'; // правило игры 
 
-const firstNamber = rendomNamber(0, 50);  // получ первое число
-const secondNamber = rendomNamber(0, 50); // получ второе число
 
+const gameRendom = () => {
+  
+  const firstNamber = rendomNamber(0, 50);  // получ первое число
+  const secondNamber = rendomNamber(0, 50); // получ второе число
 
-function NOD (firstNamber, secondNamber) { // функция считает наибольший делитель 
-	if (secondNamber > firstNamber) return NOD(secondNamber, firstNamber);
-	if (!secondNamber) return firstNamber;
-	return NOD(secondNamber, firstNamber % secondNamber);
+  const questionGame = `${firstNamber} ${secondNamber}`; 
+
+  const correctAnswer = (firstNamber, secondNamber) => {
+  if (secondNamber > firstNamber) return NOD(secondNamber, firstNamber);
+  if (!secondNamber) return firstNamber;
+  return NOD(secondNamber, firstNamber % secondNamber);
+  };
+  return [questionGame, correctAnswer]; 
 };
+
+function brainCalc() {
+  userHello(rulesGame, gameRendom);
+};
+
+export default brainCalc; 
+
 
