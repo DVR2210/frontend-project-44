bdafa934eb4bb676c83b012a1f477320efd78322
RDV2210 <altbio22@yandex.ru>
2023-01-18 14:29:25 +0300

    test

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -3,22 +3,20 @@ import userHello from './index.js';
 
 const rulesGame = 'Find the greatest common divisor of given numbers.'; // правило игры 
 
-const getGcd = (firstNamber, secondNamber) => { // функция возвращает наибольший делитель от двух чисел-работает проверил 
-  if (secondNamber > firstNamber) return getGcd (secondNamber, firstNamber);
+const getGcd = (firstNamber, secondNamber) => { // функция возвращает наибольший делитель - рабоатет 
+  if (secondNamber > firstNamber) return getGcd(secondNamber, firstNamber);
   if (!secondNamber) return firstNamber;
-  return getGcd (secondNamber, firstNamber % secondNamber);
+  return getGcd(secondNamber, firstNamber % secondNamber);
   };
 
-
-
 const gameRendom = () => {
-  
-  const firstNamber = rendomNamber(0, 50);  // получ первое число
-  const secondNamber = rendomNamber(0, 50); // получ второе число
+
+  const firstNamber = rendomNamber(0, 50); 
+  const secondNamber = rendomNamber(0, 50);
   const questionGame = `${firstNamber} ${secondNamber}`; 
   const correct = getGcd(firstNamber,secondNamber);
   const correctAnswer = String(correct);
-  
+    
   return [questionGame, correctAnswer]; 
 };
 
