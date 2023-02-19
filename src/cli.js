cfa9f1779f3ee79a787595c6c1600bc43291af14
RDV2210 <altbio22@yandex.ru>
2023-02-19 14:56:25 +0300

    mentor edit

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 function welcome() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${name}!`);
+  console.log(` Hello, ${name}!`);
 };
 
 export default welcome; 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function userHello(gameMessage, nextlavel) {
 
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${name}!`);
+  console.log(` Hello, ${name}!`);
   console.log(gameMessage); 
 
   for (let raundGame = 0; raundGame < gameLevel; raundGame += 1) { 
@@ -16,7 +16,7 @@ function userHello(gameMessage, nextlavel) {
   const playerAnswer = readlineSync.question('Your answer: '); 
    
   if (playerAnswer !== correctAnswer) { 
-    console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}\n Lets try again ${name}.`);
+    console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}\n Let's try again, ${name}.`);
     return;  
   }
   
