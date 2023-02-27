a3841cbb145c19b35da9f1a4457d18bfc83ee312
RDV2210 <altbio22@yandex.ru>
2023-02-27 10:21:24 +0300

    mentor edit

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ function welcome() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   console.log(` Hello, ${name}!`);
-}
+};
 
 export default welcome; 
