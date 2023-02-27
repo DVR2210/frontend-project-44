8cadac954c17414d0f3e6c7566632a257dab1b2b
RDV2210 <altbio22@yandex.ru>
2023-02-27 14:17:28 +0300

    mentor edit

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,16 +4,15 @@ import userHello from '../index.js';
 const rulesGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (namber) => namber % 2 === 0;
 const getQuestionAndAnswer = () => {
- const questionGame = rendomNamber(2, 100);
+  const questionGame = rendomNamber(2, 100);
 
- const correctAnswer = isEven(questionGame) ? 'yes' : 'no';
+  const correctAnswer = isEven(questionGame) ? 'yes' : 'no';
 
- return [questionGame, correctAnswer];
-
+  return [questionGame, correctAnswer];
 };
 
 function brainEvent() {
- userHello(rulesGame, getQuestionAndAnswer);
+  userHello(rulesGame, getQuestionAndAnswer);
 }
 
 export default brainEvent;
