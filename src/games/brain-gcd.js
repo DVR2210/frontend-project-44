9c8b736f57a58f8dc2550d8e0ec1f71d42a22cb2
RDV2210 <altbio22@yandex.ru>
2023-02-28 15:45:13 +0300

    mentor edit

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,17 +3,17 @@ import userHello from '../index.js';
 
 const rulesGame = 'Find the greatest common divisor of given numbers.';
 const getGcd = (firstNamber, secondNamber) => {
-  if (secondNamber > firstNamber) return getGcd(secondNamber, firstNamber);
-  if (!secondNamber) return firstNamber;
-  return getGcd(secondNamber, firstNamber % secondNamber);
+   if (secondNamber > firstNamber) return getGcd(secondNamber, firstNamber);
+   if (!secondNamber) return firstNamber;
+   return getGcd(secondNamber, firstNamber % secondNamber);
   };
 const getQuestionAndAnswer = () => {
-  const firstNamber = rendomNamber(0, 50);
-  const secondNamber = rendomNamber(0, 50);
-  const questionGame = `${firstNamber} ${secondNamber}`;
-  const correct = getGcd(firstNamber,secondNamber); 
-  const correctAnswer = String(correct);
-  return [questionGame, correctAnswer];
+   const firstNamber = rendomNamber(0, 50);
+   const secondNamber = rendomNamber(0, 50);
+   const questionGame = `${firstNamber} ${secondNamber}`;
+   const correct = getGcd(firstNamber,secondNamber); 
+   const correctAnswer = String(correct);
+   return [questionGame, correctAnswer];
 };
 function brainCalc() {
   userHello(rulesGame, getQuestionAndAnswer);
