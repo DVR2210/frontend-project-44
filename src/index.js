d1dd102de3124ec16ac2ef892fd95b59c44d717a
RDV2210 <altbio22@yandex.ru>
2023-02-28 13:14:14 +0300

    mentor edit

--- src/index.js
+++ src/index.js
@@ -3,23 +3,22 @@ const gameLevel = 3;
 
 function userHello(gameMessage, nextlavel) {
 
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log(gameMessage);
+   console.log('Welcome to the Brain Games!');
+   const name = readlineSync.question('May I have your name?');
+   console.log(`Hello, ${name}!`);
+   console.log(gameMessage);
 
-  for (let raundGame = 0; raundGame < gameLevel; raundGame += 1) {
-    сonst [questionGame, correctAnswer] = nextlavel();
-    console.log(`Question: ${questionGame}`);
-    const playerAnswer = readlineSync.question('Your answer: ');
-    if (playerAnswer !== correctAnswer) {
-    console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-    return; 
-  
-}  
-    console.log('Correct!');
- }
-    console.log(`Congratulations, ${name}!`);
- };
+   for (let raundGame = 0; raundGame < gameLevel; raundGame += 1) {
+      const [questionGame, correctAnswer] = nextlavel();
+      console.log(`Question: ${questionGame}`);
+      const playerAnswer = readlineSync.question('Your answer: ');
+      if (playerAnswer !== correctAnswer) {
+         console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+         return; 
+      }
+      console.log('Correct!');
 
+   }
+   
+}
 export default userHello;
